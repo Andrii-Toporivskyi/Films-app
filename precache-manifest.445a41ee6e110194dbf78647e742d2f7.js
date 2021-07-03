e5488d5d52bf539d496dd4646ec28d59b5be9c03
Andrii <toporivskui55@gmail.com>
2021-07-03 20:29:00 +0300

    Updates

---
+++ precache-manifest.445a41ee6e110194dbf78647e742d2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9110b0bf78f33c7c87dd46c47310117",
+    "url": "/Films-app/index.html"
+  },
+  {
+    "revision": "6d27e40f011d173150cd",
+    "url": "/Films-app/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "660ed78d04f0c2062980",
+    "url": "/Films-app/static/css/main.9824444b.chunk.css"
+  },
+  {
+    "revision": "6d27e40f011d173150cd",
+    "url": "/Films-app/static/js/2.9aa72c61.chunk.js"
+  },
+  {
+    "revision": "660ed78d04f0c2062980",
+    "url": "/Films-app/static/js/main.0f8a2ccd.chunk.js"
+  },
+  {
+    "revision": "0c1d2ae4e73c5d2da47a",
+    "url": "/Films-app/static/js/runtime~main.13044a77.js"
+  }
+]);
